fce020ddbb2b370d7e4333e7a1730f213f7a3dc6
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-05-08 17:58:47 -0500

    making a new project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hey Hows it going mane</header>
+    </div>
+  );
+}
+
+export default App;
